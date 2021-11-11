633279de10c7a2371855bc8fd65977501670010e
Python-Newbie <python100daysofcode@gmail.com>
2021-11-11 14:29:34 -0800

    New comment in db. Updating 'DATABASE_URL' to 'DATABASE_URL1' to try Heroku differences.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL1")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
